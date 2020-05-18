7e57d39d93656da9192e6984c35a66698683e486
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-18 15:47:13 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,21 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new
+    add_post(post)
+  end
+
+end
